3985d11e6d6d6d59ea994bf7aff5daea18dc8543
Rob Cho <robhcho@gmail.com>
2017-11-30 19:45:48 -0500

    Create JSON for all available products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+  def display_items
+    db_items = Product.all
+    store_items = []
+
+    db_items.each do |item|
+      items_hashes = { name: item.name,
+        price: item.price,
+        image: item.image,
+        description: item.description }
+      store_items << items_hashes
+    end
+    render json: store_items
+  end
 end
