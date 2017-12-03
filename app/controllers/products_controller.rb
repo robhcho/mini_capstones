f4aeeb978088d0f6ac640e7d10797a8b00b3cd78
Rob Cho <robhcho@gmail.com>
2017-12-03 13:22:34 -0500

    Make route using query parameter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,4 +12,10 @@ class ProductsController < ApplicationController
     end
     render json: store_items
   end
+
+  def show_single_item
+    id_number = params['item_number']
+    show_item = Product.find_by(id: id_number)
+    render json: show_item.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/online_store' => 'products#display_items'
+  get '/single_item' => 'products#show_single_item'
+  # get '/display_one/:wildcard' => 'products#show_single_item'
 end
