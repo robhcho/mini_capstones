39ec86d9ad51ed1ef278f457d0814cb9c63fbdb4
Rob Cho <robhcho@gmail.com>
2017-12-03 13:24:57 -0500

    Make route utilizing url segment params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/online_store' => 'products#display_items'
   get '/single_item' => 'products#show_single_item'
-  # get '/display_one/:wildcard' => 'products#show_single_item'
+  get '/display_one/:item_number' => 'products#show_single_item'
 end
