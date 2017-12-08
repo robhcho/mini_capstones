ef3db2c450261f99dd17a15ec26d6ee017205302
Rob Cho <robhcho@gmail.com>
2017-12-07 20:50:36 -0500

    rewrite code to be more efficient

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,31 +1,31 @@
 class Product < ApplicationRecord
   def as_json
     {
+      id: id,
       name: name,
       price: price,
       image: image,
       description: description,
-      discounted: is_discounted,
+      discounted?: is_discounted?,
       tax: tax,
       total: total
     }
   end
 
-  def is_discounted
-    if price < 2
-      return true
-    else
-      false
-    end
+  def is_discounted?
+    price < 2
+    # if price < 2
+    #   true
+    # else
+    #   false
+    # end
   end
 
   def tax
-    tax = price * 0.09
-    p tax
+    price * 0.09
   end
 
   def total
-    total = price + tax
-    p total
+    price + tax
   end
 end
