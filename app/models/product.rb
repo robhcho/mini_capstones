d75a166d86bc45dde210d905994f8592b3fd631d
Rob Cho <robhcho@gmail.com>
2017-12-07 20:27:55 -0500

    Make model methods as_json, is_discounted, tax, total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,31 @@
 class Product < ApplicationRecord
+  def as_json
+    {
+      name: name,
+      price: price,
+      image: image,
+      description: description,
+      discounted: is_discounted,
+      tax: tax,
+      total: total
+    }
+  end
+
+  def is_discounted
+    if price < 2
+      return true
+    else
+      false
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+    p tax
+  end
+
+  def total
+    total = price + tax
+    p total
+  end
 end
